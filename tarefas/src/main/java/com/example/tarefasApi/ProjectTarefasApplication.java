f342d5dbf59310e7088c11bd06736190c68dc9ae
Guilherme Silva <sgsouza.173@gmail.com>
2024-08-11 10:14:24 -0300

    Trazendo os arquivos

---
+++ tarefas/src/main/java/com/example/tarefasApi/ProjectTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarefasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTarefasApplication.class, args);
+	}
+
+}
---
+++ tarefas/src/test/java/com/example/tarefasApi/ProjectTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarefasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
